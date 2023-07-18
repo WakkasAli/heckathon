bcea7f4f5e450b645004d8fc077099ebdce89364
waqas ali <aliwaqas892@gmail.com>
2023-07-19 01:11:24 +0500

    cartpagechanges

--- src/app/ProductCart/page.tsx
+++ src/app/ProductCart/page.tsx
@@ -1,37 +1,70 @@
 import React from 'react'
 import { client } from "../../../sanity/lib/client"
+import { drizzle } from 'drizzle-orm/vercel-postgres'
+import { InferModel, eq, sql } from 'drizzle-orm'
+import { integer, pgTable, serial, text, timestamp,varchar } from 'drizzle-orm/pg-core'
+import { Pool } from 'pg'
+import { VercelPool, VercelPostgresClientConfig } from '@vercel/postgres'
+import { cartTable } from '@/lib/drizzle'
+import { cookies } from 'next/headers'
+import CartItems from '@/components/CartItems'
 
+const pool = new Pool({
+  connectionString: 'postgres://default:gvQ6ZW7aBenV@ep-orange-art-033162.us-east-1.postgres.vercel-storage.com:5432/verceldb',
+  ssl:true,
+});
+
+const db = drizzle(pool as VercelPool);
 
 export const getProducts = async ()=> {
   //const res = await client.fetch('*[_type=="product"]{_id,price,pname,category,"image":image[].asset->url}')
+  const uid = cookies().get("user_id")?.value;
+  // const res = await fetch('http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}')
+  const res = await db.select().from(cartTable).where(eq(cartTable.user_id, uid as string));
   
-  const res = await fetch('http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}')
-  return res.json()
+  return res
 } 
 
 interface ICart{
-  ID:string,
-  USER_ID:string,
-  PRODUCT_ID:string,
+  id:number,
+  user_id:string,
+  product_id:string,
   quantity:number
 }
 
+
 const getallCart = async () => {
-  const data = await getProducts()
-console.log("cart")
+  const data:ICart[] = await getProducts()
+  
+console.log(data)
+
   return(
-    <div className="flex justify-evenly mt-16">
+    <div className="mt-8">
       {
-        // data.map((item: ICart)=>
-        // <div>
-        //   <div>
-        //     {item.ID}
-        //   </div>
-        //   <div className="text-lg"><b>{item.USER_ID}</b></div>
-        //   <div className="text-lg"> Price: ${item.PRODUCT_ID}</div>
-        // </div>
-        //)
-        <h1>Working</h1>
+        
+        data.map((item: ICart)=>
+        <div>
+          
+        
+          <div className="flex md:py-4">
+            <div className="px-4 sm:w-1/5 md:w-1/5 xl:w-1/5">
+            </div>
+            
+            <div className="px-4 sm:w-1/2 md:w-1/2 xl:w-1/2">
+            <div className='h1 text-1lg font-bold'>
+            Shopping Cart
+            </div>
+            {/* <div className="text-lg"> Price: ${item.product_id}</div> */}
+            <CartItems item={item.product_id} />
+            </div>
+
+            <div className="px-4 sm:w-1/3 md:w-1/3 xl:w-1/3">
+              <div className="text-lg"><b>Order Summary</b></div>
+            </div>
+          </div>
+        </div>
+        )
+        
       }
     </div>
   )
---
+++ src/components/CartItems.tsx
@@ -0,0 +1,73 @@
+"use client"
+import * as React  from 'react'
+import ProductCard from "@/components/productCard"
+import P1 from "/public/p1.png"
+import P2 from "/public/p2.png"
+import P3 from "/public/p3.png"
+import Link from "next/link"
+import ProductQuantity from '@/components/ProductQuantity'
+import AddtoCartbutton from '@/components/AddtoCartbutton'
+import { client } from '../../sanity/lib/client'
+
+
+export const getProductsbyId = async (productId:any)=> {
+    
+  const res = await client.fetch('*[_type=="product" &&  _id == $productId]{_id,price,pname,pdetail,pcare,category,"image":image[].asset->url}',{productId:productId})
+  console.log(res)
+  return res;
+} 
+
+interface IProduct{
+  _id:string,
+  pname:string,
+  pdetail: string,
+  pcare: string,
+  image:string,
+  price:number,
+  category:string,
+  quantity:number
+}
+
+const getproductdetail = async(params:any) => {
+  
+  const data:IProduct[] = await getProductsbyId(params.item)
+
+
+  return(
+    <div className="flex mt-4">
+      {
+        data.map((item: IProduct)=>
+        <div className="flex">
+          
+            <div className="px-4 sm:w-1/7 md:w-1/7 xl:w-1/7 ">
+                <img src={item.image[0]} width={180} height={100} alt='' />
+            </div>
+
+            <div className="px-4 sm:w-1/2 md:w-1/2 xl:w-1/2 ">
+            
+          <div className="text-lg"><b>{item.pname}</b></div>
+          <div className="text-gray-500"><b>{item.category}</b></div>
+
+            <div className='py-4'>
+                <b>Delevery Estimation</b>
+            </div>
+            <div className='text-yellow-500'>
+                <b>5 Working Days</b>
+            </div>
+
+            <div className='flex py-4 text-xl'>
+                <span className='px-3 item-start'> <b>${item.price}.00</b></span>
+                <span className='item-end'>
+                <ProductQuantity item={item} />
+                </span>
+            </div>
+          </div>
+        </div>
+        )
+      }
+    </div>
+  )
+}
+  
+
+export default getproductdetail
