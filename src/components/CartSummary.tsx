7b92d42d841ed6aab1d0687d9100cdae0b55d106
waqas ali <aliwaqas892@gmail.com>
2023-07-22 00:49:03 +0500

    Running

--- src/components/CartSummary.tsx
+++ src/components/CartSummary.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { getStripe } from "@/lib/strip";
+import  getStripe  from "@/lib/strip";
 
 
 import {Button} from "@/components/ui/button"
@@ -10,9 +10,10 @@ import { FC } from "react";
 const CartSummary:FC<{item: any}>=({item})=>{
 
     const handleCheckout = async () =>{
+      alert("entered")
         console.log("Cart Summary Page: ");
         const stripe = await getStripe();
-      
+        alert("Entered"+stripe)
         const response = await  fetch(`http://localhost:3000/api/checkout`,{
         method:"POST",
         headers: { "Content-Type": "application/json" },
@@ -22,19 +23,27 @@ const CartSummary:FC<{item: any}>=({item})=>{
         })
       })
       
+      
+      
       if(response.status ===500) return;
       
       const data = await response.json()
+      console.log(data.url)
       stripe.redirectToCheckout({sessionId: data.id})
       
         
       }
 
       return(
-        
-        <Button onClick={ ()=> handleCheckout()}>
+        <div>
+        <Button onClick={async ()=> await handleCheckout()}>
             Process to Checkout
         </Button>
+
+<Button onClick={ ()=> alert("Text")}>
+Testing
+</Button>
+</div>
     )
 }
 
--- src/lib/strip.ts
+++ src/lib/strip.ts
@@ -2,10 +2,12 @@ import { loadStripe } from "@stripe/stripe-js";
 
 let stripePromise: any;
 
-export const getStripe = () => {
-  // check if there is any stripe instance
-  
-    stripePromise = loadStripe(process.env.NEXt_PUBLIC_STRIPE_PUBLIC_KEY!);
-  
+const getStripe = () => {
+  if(!stripePromise) {
+      stripePromise = loadStripe("pk_test_51JMcJ3L0iWxuD9W8j5twsUg19q6Pr9P9XfKQI83TjmEtxLlZVKlbRsB5s6u4tz848sEGMTrEDqLlLdVzs5S9EhBH00gXRK0UfD");
+  }
+console.log(stripePromise)
   return stripePromise;
-};
+}
+
+export default getStripe
