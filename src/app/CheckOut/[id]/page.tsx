156730cc99f1fc613d4992d61ee22d20228c786a
Wakkas Ali <w.ali@pmtac.com>
2023-07-21 10:37:04 +0500

    new changes

--- src/app/CheckOut/[id]/page.tsx
+++ src/app/CheckOut/[id]/page.tsx
@@ -1,35 +1,15 @@
+"use client"
 import React from 'react'
-import { drizzle } from 'drizzle-orm/vercel-postgres'
-import { eq, param } from 'drizzle-orm'
-import { Pool } from 'pg'
-import { VercelPool } from '@vercel/postgres'
-import { cartTable } from '@/lib/drizzle'
-import { cookies } from 'next/headers'
-import { Stripe } from 'stripe'
-import { stripe } from "@/lib/stripe";
-import { NextResponse } from 'next/server'
-
-const pool = new Pool({
-  connectionString: 'postgres://default:gvQ6ZW7aBenV@ep-orange-art-033162.us-east-1.postgres.vercel-storage.com:5432/verceldb',
-  ssl:true,
-});
+import { useShoppingCart } from 'use-shopping-cart'
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+import getStripe from '@/lib/publicStripe';
 
 
-const db = drizzle(pool as VercelPool);
-
-export const getProducts = async ()=> {
-  //const res = await client.fetch('*[_type=="product"]{_id,price,pname,category,"image":image[].asset->url}')
-  // const res = await fetch('http://localhost:3000/api/cart?user_id=${cookies().get("user_id")?.value}')
-
-  const uid = cookies().get("user_id")?.value;
-  const res = await db.select().from(cartTable).where(eq(cartTable.user_id, uid as string));
-  return res
-} 
 
 async function oncheckout(apiId:any) {
   console.log("checkout Method with apiId:  ", apiId)
   
-  let response: any =  fetch(`http://localhost:3000/api/checkout`,{
+  const response: any = await  fetch(`http://localhost:3000/api/checkout`,{
     method:"POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -38,15 +18,22 @@ async function oncheckout(apiId:any) {
     })
   })
   
-  console.log("Api Response:  ",response.url)
+   const data = await (response).json()
 
-   let {dataa} = await (await response).json()
+   console.log("Response Data:  ",data)
+  const stripe = await getStripe();
+  const {error} = await stripe!.redirectToCheckout({
+    sessionId:data.id,
+  });
+  
+// const {redirectToCheckout} = useShoppingCart();
+  
+//   console.log("Api Response:  ",response.url)
 
-  // window.location.href(dataa.ur);
-  // const result = await redirectToCheckout(data.id)
-  // if(result?.error){
-  //   console.error(result)
-  // }
+//   let {data} = await (await response).json()
+//   console.log("SessionId: ",data)
+//   const result = await redirectToCheckout(data.id)
+  
 }
 
 interface ICart{
@@ -59,15 +46,12 @@ interface ICart{
 
 const checkout = async ({params}:any) => {
 
-  const data:ICart[] = await getProducts()
   oncheckout(params)
-  console.log("CheckOut Data Length:  ", data.length)
 
   return(
     <div className="mt-8">
       {
         
-        data.map((item: ICart)=>
         <div>
           
         
@@ -85,7 +69,7 @@ const checkout = async ({params}:any) => {
 
           </div>
         </div>
-        )
+        
         
       }
     </div>
